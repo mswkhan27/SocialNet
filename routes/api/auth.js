27a02618df741bfe1d9176e24d6d5b7a826e8f4b
Shehroz <mswkhan27@gmail.com>
2021-03-31 23:42:25 +0500

    enter

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -12,16 +12,14 @@ const User = require('../../models/User');
 // @desc Test routeY
 //@access Public
 
-router.get('/',auth, async(req,res)=>{
-
-    try{
-        const user=await User.findById(req.user.id).select('-password');
-        console.log(user);
-     }
-    catch(err){
-        console.error(err.message);
-        res.status(500).send("Server Error");
+router.get('/', auth, async (req, res) => {
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      res.json(user);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
     }
-});
+  });
 
 module.exports=router;
--- server.js
+++ server.js
@@ -1,6 +1,20 @@
-const express= require('express')
+const express= require('express');
+const connectDB = require('./config/db');
 const app=express();
+
+connectDB();
+//Init Middleware
+
+app.use(express.json({extended:false}));
+
+
 app.get('/',(req,res)=>res.send('API Running'));
+
+
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/posts',require('./routes/api/posts'));
+app.use('/api/profile',require('./routes/api/profile'));
+app.use('/api/auth',require('./routes/api/auth'));
 
 const PORT=process.env.PORT || 5003;
 app.listen(PORT,()=>console.log('Server has started port, ${PORT}'));
